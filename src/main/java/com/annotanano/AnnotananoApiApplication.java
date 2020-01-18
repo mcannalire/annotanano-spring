104d0002fb431b118c7b0052cd333472524cb5a8
mcannalire <lokad90@gmail.com>
2020-01-18 14:04:02 +0100

    Add files via upload

--- src/main/java/com/annotanano/AnnotananoApiApplication.java
+++ src/main/java/com/annotanano/AnnotananoApiApplication.java
@@ -7,18 +7,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
 @SpringBootApplication
 @RestController
 public class AnnotananoApiApplication {
 	
 	@PostMapping
 	public String login(@RequestBody String userName) {
+
 		return "LOGIN EFFETTUATO CON " + userName;
 	}
 	
 	@GetMapping
 	public String login() {
-		return "LOGIN EFFETTUATO MARCO";
+		MongoClientURI uri = new MongoClientURI(
+			    "mongodb+srv://lokad90:mongodb@cluster0-biuot.mongodb.net/test?retryWrites=true&w=majority");
+			
+		com.mongodb.MongoClient mongoClient = new com.mongodb.MongoClient(uri);
+		MongoDatabase database = mongoClient.getDatabase("test");
+		
+		
+		return "LOGIN EFFETTUATO MARCO, nome database mongo: " + database.getName();
 	}
 
 	public static void main(String[] args) {
