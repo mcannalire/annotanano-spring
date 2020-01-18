3cd4b713e7130b2d7723fd3a0d3faef79b40bf02
mcannalire <lokad90@gmail.com>
2020-01-18 13:32:04 +0100

    Update AnnotananoApiApplication.java

--- src/main/java/com/annotanano/AnnotananoApiApplication.java
+++ src/main/java/com/annotanano/AnnotananoApiApplication.java
@@ -2,6 +2,7 @@ package com.annotanano;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,11 @@ public class AnnotananoApiApplication {
 	@PostMapping
 	public String login(@RequestBody String userName) {
 		return "LOGIN EFFETTUATO CON " + userName;
+	}
+	
+	@GetMapping
+	public String login() {
+		return "LOGIN EFFETTUATO MARCO";
 	}
 
 	public static void main(String[] args) {
