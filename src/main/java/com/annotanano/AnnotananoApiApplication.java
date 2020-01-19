707018bf6cc15f32b0cf771116559f6f2f94a0c1
mcannalire <lokad90@gmail.com>
2020-01-19 10:41:50 +0100

    Add files via upload

--- src/main/java/com/annotanano/AnnotananoApiApplication.java
+++ src/main/java/com/annotanano/AnnotananoApiApplication.java
@@ -1,8 +1,12 @@
 package com.annotanano;
 
+import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 @SpringBootApplication
@@ -22,9 +27,25 @@ public class AnnotananoApiApplication {
 	
 	@PostMapping
 	public User login(@RequestBody UserCredential uCred) {
-		
 		User user = new User();
-		user.setUserId("0000");
+		
+		MongoDatabase db = getMongoDb();
+		
+		MongoCollection<Document> collection = db.getCollection("users");
+		Bson condition = new Document("$eq", uCred.getUserName());
+		Bson filter = new Document("userName", condition);
+		
+		Document document = collection.find(filter).first();
+		
+		if(document != null) {
+			if(document.getString("pwd").equals(uCred.getPwd()))
+				user.setUserId(document.getString("userId"));
+			else
+				user.setUserId("NA");
+		} else {
+			user.setUserId("NA");
+		}
+		
 		return user;
 	}
 	
@@ -43,6 +64,15 @@ public class AnnotananoApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnnotananoApiApplication.class, args);
+	}
+	
+	private MongoDatabase getMongoDb() {
+		MongoClientURI uri = new MongoClientURI(
+			    "mongodb+srv://lokad90:mongodb@cluster0-biuot.mongodb.net/test?retryWrites=true&w=majority");
+			
+		com.mongodb.MongoClient mongoClient = new com.mongodb.MongoClient(uri);
+		MongoDatabase database = mongoClient.getDatabase("annotananodb");
+		return database;
 	}
 
 }
